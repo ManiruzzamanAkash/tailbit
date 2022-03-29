bdc572b24ea86f3495abdfb8564e72b15df0a9fc
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-03-29 15:55:41 +0600

    Added packages setup

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { DefinePlugin } = require('webpack');
+
+const config = {
+    entry: {
+        app: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+    },
+    externals: {
+        '@wordpress/api-fetch': ['wp', 'apiFetch'],
+        '@wordpress/i18n': ['wp', 'i18n'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    'postcss-loader'
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.jpg$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.jpeg$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            mimetype: 'image/png',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            'react-dom': '@hot-loader/react-dom',
+        },
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
+        },
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods':
+                'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+            'Access-Control-Allow-Headers':
+                'X-Requested-With, content-type, Authorization',
+        },
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new DefinePlugin({
+            'process.env.FORCE_REDUCED_MOTION': JSON.stringify(
+                process.env.FORCE_REDUCED_MOTION
+            ),
+            process: { env: {} }
+        }),
+    ],
+};
+
+module.exports = (env, argv) => {
+    if (argv.hot) {
+        // Cannot use 'contenthash' when devServer reloading is enabled.
+        config.output.filename = '[name].js';
+    }
+
+    return config;
+};
